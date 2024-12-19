2bad9356f446b61ace34722dd9ed34074aeceee5
NoobLord02 <114958932+sqeak20000@users.noreply.github.com>
2024-12-19 16:56:53 -0600

    Moving Average and One Measurement Gravity

--- src/main/java/frc/robot/Fusion2d.java
+++ src/main/java/frc/robot/Fusion2d.java
@@ -1,5 +1,12 @@
 package frc.robot;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.plaf.basic.BasicScrollPaneUI.VSBChangeListener;
+
+import org.opencv.core.Size;
+
 import com.ctre.phoenix6.hardware.Pigeon2;
 
 import edu.wpi.first.cscore.VideoMode;
@@ -8,18 +15,24 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.Odometry;
-import edu.wpi.first.wpilibj.Timer;
 
 public class Fusion2d {
     private Pigeon2 pigeon;
     private Pose2d pose = new Pose2d();
+    private MovingAverage acc_x = new MovingAverage(5);
+    private MovingAverage acc_y = new MovingAverage(5);
+    private MovingAverage rot = new MovingAverage(5);
     private double[] linearVelocity = { 0, 0 };
     private double[] linearPosition = { 0, 0 };
+    private double[] gravityVector = { 0, 0, 0 };
     private float loopTime = 0.02f;
     private double yawOffset = 0.0d;
 
     public Fusion2d(Pigeon2 pigeon) {
         this.pigeon = pigeon;
+        this.gravityVector[0] = this.pigeon.getGravityVectorX().getValueAsDouble();
+        this.gravityVector[1] = this.pigeon.getGravityVectorY().getValueAsDouble();
+        this.gravityVector[2] = this.pigeon.getGravityVectorZ().getValueAsDouble();
     }
 
     public void resetPosition(Rotation2d rotation, Pose2d pose) {
@@ -36,20 +49,55 @@ public class Fusion2d {
     // u = u + a * dt;
 
     public void update() {
-        this.linearVelocity[0] = this.linearVelocity[0]
-                + (this.pigeon.getAccelerationX().getValueAsDouble() - this.pigeon.getGravityVectorX().getValueAsDouble()) * 9.80665 * this.loopTime;
+        System.out.println("(" + this.pigeon.getGravityVectorX() + ", " + this.pigeon.getGravityVectorZ() + ", "
+                + this.pigeon.getGravityVectorY() + ")");
+
+        acc_x.next(
+                this.pigeon.getAccelerationX().getValueAsDouble() - this.gravityVector[0]);
+        acc_y.next(
+                this.pigeon.getAccelerationY().getValueAsDouble() - this.gravityVector[1]);
+        rot.next(this.pigeon.getYaw().getValueAsDouble());
+
+        this.linearVelocity[0] = this.linearVelocity[0] + (acc_x.get()) * 9.80665 * this.loopTime;
         this.linearPosition[0] = this.linearPosition[0] + this.linearVelocity[0] * this.loopTime;
 
-        this.linearVelocity[1] = this.linearVelocity[1]
-                + (this.pigeon.getAccelerationY().getValueAsDouble() - this.pigeon.getGravityVectorY().getValueAsDouble()) * 9.80665 * this.loopTime;
+        this.linearVelocity[1] = this.linearVelocity[1] + (acc_y.get()) * 9.80665 * this.loopTime;
         this.linearPosition[1] = this.linearPosition[1] + this.linearVelocity[1] * this.loopTime;
 
         this.pose = new Pose2d(this.linearPosition[0], this.linearPosition[1],
-                Rotation2d.fromDegrees(this.pigeon.getYaw().getValueAsDouble() + this.yawOffset));
+                Rotation2d.fromDegrees(rot.get() + this.yawOffset));
     }
 
     public Pose2d getPoseMeters() {
         return this.pose;
     }
 
+}
+
+class MovingAverage {
+
+    private int windowSize;
+    private Queue<Double> window;
+    private double sum;
+
+    public MovingAverage(int windowSize) {
+        this.windowSize = windowSize;
+        this.window = new LinkedList<>();
+        this.sum = 0.0d;
+    }
+
+    public double next(double value) {
+        window.add(value);
+        sum += value;
+
+        if (window.size() > windowSize) {
+            sum -= window.remove();
+        }
+
+        return sum / window.size();
+    }
+
+    public double get() {
+        return sum / window.size();
+    }
 }
